d25f238705fbf7a154a771f638fccbd372ae6786
Robin Heathcote <robin@createk.io>
2016-09-23 16:33:41 +0100

    Adds API request and log messages

--- SearchPage.js
+++ SearchPage.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import { NavigatorIOS, AppRegistry, StyleSheet, Text, TextInput, View,
-  TouchableHighlight, ActivityIndicatorIOS, Image }
+  TouchableHighlight, ActivityIndicator, Image }
   from 'react-native';
 
   var styles = StyleSheet.create({
@@ -79,7 +79,8 @@ import { NavigatorIOS, AppRegistry, StyleSheet, Text, TextInput, View,
       super(props);
       this.state = {
         searchString: 'london',
-        isLoading: false
+        isLoading: false,
+        message: ''
       };
     }
 
@@ -90,6 +91,23 @@ import { NavigatorIOS, AppRegistry, StyleSheet, Text, TextInput, View,
     _executeQuery(query) {
       console.log(query);
       this.setState({ isLoading : true });
+      fetch(query)
+        .then(response => response.json())
+        .then(json => this._handleResponse(json.response))
+        .catch(error =>
+          this.setState({
+            isLoading: false,
+            message: 'Something bad happened ' + error
+          }));
+    }
+
+    _handleResponse(response) {
+      this.setState({ isLoading: false , message: ''});
+      if (response.application_response_code.substr(0, 1) === '1') {
+        console.log('Properties found: ' + response.listings.length);
+      } else {
+        this.setState({ message: 'Location not recognized, please try again.'});
+      }
     }
 
     onSearchPressed() {
@@ -99,7 +117,7 @@ import { NavigatorIOS, AppRegistry, StyleSheet, Text, TextInput, View,
 
     render() {
       var spinner = this.state.isLoading?
-        ( <ActivityIndicatorIOS
+        ( <ActivityIndicator
             size='large'/> ) :
         ( <View/>);
       return (
@@ -130,6 +148,7 @@ import { NavigatorIOS, AppRegistry, StyleSheet, Text, TextInput, View,
         </TouchableHighlight>
         <Image source={require('./Resources/house.png')} style={styles.image}/>
         {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
       );
     }
